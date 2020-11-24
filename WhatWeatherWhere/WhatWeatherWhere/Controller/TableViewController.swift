11e62a2989d1c7f5810b4c90fa05fef4c7a4fc6d
Sebastian Berchtold <seberchtold@gmail.com>
2020-11-24 14:05:55 +0100

    Deleting doesnt function

--- WhatWeatherWhere/WhatWeatherWhere/Controller/TableViewController.swift
+++ WhatWeatherWhere/WhatWeatherWhere/Controller/TableViewController.swift
@@ -13,12 +13,23 @@ class TableViewController: UITableViewController, NSFetchedResultsControllerDele
     var fetchedWeatherController: NSFetchedResultsController<WeatherData>!
     var annotations = [Pin]()
     var selectedIndex = 0
+    let searchController = UISearchController(searchResultsController: nil)
+    var filteredPins: [Pin] = []
+    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.searchController = self.searchController
         tableView.rowHeight = UITableView.automaticDimension
         setupFetchedResultsControllerPin()
         setupFetchedResultsControllerWeatherData()
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.placeholder = "Search Dates"
+        annotations = fetchedResultsController.fetchedObjects ?? DataModelPins.pins
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+        
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -55,22 +66,25 @@ class TableViewController: UITableViewController, NSFetchedResultsControllerDele
             print("There was a problem with fetching: \(error.localizedDescription)")
         }
     }
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "showDetail" {
-            let vc = segue.destination as! WeatherDetailViewController
-            if let data = fetchedResultsController.fetchedObjects{
-                print("1st")
-                vc.weather = data[selectedIndex].weather
-                
-            }
-        }
-    }
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
+    
+  
+
 }
 
-extension TableViewController{
+extension TableViewController: UISearchResultsUpdating{
+    func updateSearchResults(for searchController: UISearchController) {
+            if let searchText = searchController.searchBar.text, !searchText.isEmpty {
+                if let data = fetchedResultsController.fetchedObjects{
+                    data.filter{ (pin) in
+                       return (pin.weather?.name?.contains(searchText) ?? true)
+                    }
+                }
+            }
+            self.tableView.reloadData()
+      }
        
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return fetchedResultsController.fetchedObjects?.count ?? DataModelPins.pins.count
@@ -79,19 +93,34 @@ extension TableViewController{
         setupFetchedResultsControllerWeatherData()
         let dateFormatter = DateFormatter()
         let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! CustomTableCell
-        if let data = fetchedResultsController.fetchedObjects{
+        /*if let data = fetchedResultsController.fetchedObjects{
             dateFormatter.dateFormat = "yyyy-MM-dd hh:mm:ss"
             cell.locationLabel?.text = data[indexPath.item].weather?.name
             cell.dateLabel?.text = dateFormatter.string(from: data[indexPath.item].date ?? Date())
-        }
+        }*/
+        dateFormatter.dateFormat = "yyyy-MM-dd hh:mm:ss"
+        cell.locationLabel?.text = annotations[indexPath.item].weather?.name
+        cell.dateLabel?.text = dateFormatter.string(from: annotations[indexPath.item].date ?? Date())
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         selectedIndex = indexPath.item
-        print("2nd")
-        performSegue(withIdentifier: "showDetail", sender: nil)
-        tableView.deselectRow(at: indexPath, animated: true)
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "id") as! WeatherDetailViewController
+        if let data = fetchedResultsController.fetchedObjects{
+            vc.weather = data[selectedIndex].weather
+            tableView.deselectRow(at: indexPath, animated: true)
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+         //   appDelegate.dataController.viewContext.delete(annotations[indexPath.item] as! NSManagedObject)
+            print("Delete", annotations[indexPath.row])
+            annotations.remove(at: indexPath.row)
+         //   try? appDelegate.dataController.viewContext.save()
+            self.tableView.deleteRows(at: [indexPath], with: .fade)
+        }
     }
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 100.0
--- WhatWeatherWhere/WhatWeatherWhere/Controller/WeatherDetailViewController.swift
+++ WhatWeatherWhere/WhatWeatherWhere/Controller/WeatherDetailViewController.swift
@@ -6,9 +6,27 @@ import Foundation
 import UIKit
 class WeatherDetailViewController: UITableViewController{
     var weather: WeatherData?
-    
-    override func viewDidLoad() {
+    var weatherTitle = [
+        "Name",
+        "Beschreibung",
+        "Temperatur",
+        "Max-Temperatur",
+        "Mindest-Temperatur",
+    ]
+    var weatherData: [ String] = []
+   override func viewDidLoad() {
         super.viewDidLoad()
+    weather?.tempMin = weather?.tempMin ?? 0 - 273.15
+    weather?.tempMax = weather?.tempMax ?? 0 - 273.15
+    weather?.tempNow = weather?.tempNow ?? 0 - 273.15
+    weatherData.append(weather?.name ?? "")
+    weatherData.append(weather?.descriptionOfWeather ?? "")
+    weatherData.append(weather?.tempNow.description ?? "")
+    weatherData.append(weather?.tempMax.description ?? "")
+    weatherData.append(weather?.tempMin.description ?? "")
+    }
+    
+    func convertObjectToArray(){
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -16,13 +34,13 @@ class WeatherDetailViewController: UITableViewController{
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "detailCell", for: indexPath) as! CustomDetailCell
-        cell.titleLabel.text = "Tempretature"
-        cell.valueLabel.text = weather?.tempNow.description
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! CustomDetailCell
+        cell.titleLabel.text = weatherTitle[indexPath.row]
+        cell.valueLabel.text = weatherData[indexPath.row]
         return cell
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return weatherTitle.count
     }
     
     
